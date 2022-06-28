3d564e11e2ea859d0d009eac9b8a5f15c1f50619
JungHo-K1m <62466374+JungHo-K1m@users.noreply.github.com>
2022-06-28 16:26:24 +0900

    단순 연산, 단순 비교

--- src/ev.java
+++ src/ev.java
@@ -1,3 +1,6 @@
+import javax.lang.model.type.ArrayType;
+import javax.swing.table.AbstractTableModel;
+import java.util.Arrays;
 import java.util.Scanner;
 import java.util.Stack;
 
@@ -7,11 +10,11 @@ public class ev {
         String[] oper1 = {"+", "-", "*", "/", "%"};   //사칙연산
         String[] oper2 = {"==", "!=", "<", "<=", ">", ">="};  //비교연산
         String[] oper3 = {"!", "&&", "||"};   //논리연산
+        String op = "+-*/%!=<>&|";
 
         Stack<Integer> values = new Stack<Integer>();   //숫자를 넣을 스택
-        Stack<Character> operations = new Stack<Character>();   //사칙연산자를 넣을 스택
-        Stack<Boolean> result = new Stack<Boolean>();     //논리, 비교연산 결과를 넣을 스택
-        Stack<String> bool = new Stack<String>();    //비교연산자, 논리연산자, 논리 결과를 넣을 스택
+        Stack<String> operations = new Stack<String>();   //연산자를 넣을 스택
+        Stack<Boolean> bool = new Stack<Boolean>();     //논리/비교 결과를 넣을 스택
 
 
         //입력한 문자열 길이만큰 반복
@@ -32,66 +35,90 @@ public class ev {
             }
 
             //읽은 문자가 시작하는 괄호인 경우
-            else if(token[i] == '(' && !Character.isDigit(token[i]))
-                operations.push(token[i]);  //연산자를 넣는 스택에 넣자
+            else if(token[i] == '(') {
+                operations.push(String.valueOf(token[i]));  //연산자를 넣는 스택에 넣자
+            }
 
             //읽은 문자가 닫는 괄호인 경우
-            else if(token[i] == ')' && !Character.isDigit(token[i])){
-                while (operations.peek() != '(')    //연산스택의 마지막에 들어간 값이 시작 괄호가 아니면,
-                    values.push(applyOp(operations.pop(), values.pop(), values.pop())); // 괄호 안의 내용을 계산하여 결과값을 숫자 스택에 넣는다.
+            else if(token[i] == ')'){
+                //연산스택의 마지막에 들어간 값이 시작 괄호가 아니고
+                while (operations.peek() != "(") {
+                    //연산스택의 마지막 값이 사칙연산자면
+                    if(Arrays.asList(oper1).contains(operations.peek()) && hasPrecedence(token[i], operations.peek()))
+                        values.push(applyOp(operations.pop(), values.pop(), values.pop())); // 괄호 안의 내용을 계산하여 결과값을 숫자 스택에 넣는다.
+                    //연산스택의 마지막 값이 비교연산자면
+                    else if(Arrays.asList(oper2).contains(operations.peek()))
+                        bool.push(cmpOp(operations.pop(), values.pop(), values.pop())); //숫자를 비교히여 결과값을 스택에 넣는다.
+                    //연산스택의 마지막 값이 논리연산자면
+                    else if(Arrays.asList(oper3).contains(operations.peek())&& !bool.empty())
+                        bool.push(logicOp(operations.pop(), bool.pop(), bool.pop()));   //두 값을 비교하여 결과값을 스택에 넣는다.
+                }
                 operations.pop();   //연산자 스택의 마지막 문자'(' 반환
             }
 
             //토큰의 i번째 문자가 연산자인 경우
-            else if(token[i] != '(' && !Character.isDigit(token[i])){
+            else if(op.indexOf(token[i]) != -1){
                 StringBuffer operBuffer = new StringBuffer();
 
-                while(i < token.length && !Character.isDigit(token[i]))
+                while(i < token.length && !Character.isDigit(token[i]) )
                    operBuffer.append(token[i++]); //다음 토큰의 문자를 연산버퍼에 저장
 
-                bool.push(String.valueOf(operBuffer));  //연산버퍼를 스트링으로 바꿔 스택에 저장
+                operations.push(String.valueOf(operBuffer));  //연산버퍼를 스트링으로 바꿔 스택에 저장
 
                 i--;
+
+                if(Arrays.asList(oper1).contains(operations.peek()) && hasPrecedence(token[i], operations.peek()))
+                    values.push(applyOp(operations.pop(), values.pop(), values.pop())); // 괄호 안의 내용을 계산하여 결과값을 숫자 스택에 넣는다.
+                    //연산스택의 마지막 값이 비교연산자면
+                else if(Arrays.asList(oper2).contains(operations.peek()))
+                    bool.push(cmpOp(operations.pop(), values.pop(), values.pop())); //숫자를 비교히여 결과값을 스택에 넣는다.
+                    //연산스택의 마지막 값이 논리연산자면
+                else if(Arrays.asList(oper3).contains(operations.peek())&& !bool.empty())
+                    bool.push(logicOp(operations.pop(), bool.pop(), bool.pop()));   //두 값을 비교하여 결과값을 스택에 넣는다.
             }
         }
 
         //연산자 스택이 비어있지 않은 경우(마지막 계산)
-        while (!operations.empty() && oper)
-            values.push(applyOp(operations.pop(), values.pop(), values.pop())); //값을 계산하여 숫자 스택에 넣는다
-
-        String rtn = String.valueOf(values.pop());
+        while (!operations.empty()){
+            if(Arrays.asList(oper1).contains(operations.peek()))
+                values.push(applyOp(operations.pop(), values.pop(), values.pop())); //값을 계산하여 숫자 스택에 넣는다
+            else if(Arrays.asList(oper2).contains(operations.peek()))
+                bool.push(cmpOp(operations.pop(), values.pop(), values.pop())); //숫자를 비교히여 결과값을 스택에 넣는다.
+            else if(Arrays.asList(oper3).contains(operations.peek()) && !bool.empty())
+                bool.push(logicOp(operations.pop(), bool.pop(), bool.pop()));
+        }
 
         //계산 결과 반환
-        return rtn;
+        return (values.empty())?(String.valueOf(bool.pop())):(String.valueOf(values.pop()));
     }
 
 
     //우선순위 확인
-    public static boolean hasPrecedence(char op1, char op2){
+    public static boolean hasPrecedence(char op1, String op2){
         //두번째로 받은 문자(연산자 스택의 마지막 문자)
-        if (op2 == '(' || op2 == ')')
+        if (op2 == "(" || op2 == ")")
             return false;
         //첫 번째로 받은 문자(토큰의 i번째 문자)
-        if ((op1 == '*' || op1 == '/') && (op2 == '+' || op2 == '-'))
+        if ((op1 == '*' || op1 == '/') && (op2 == "+" || op2 == "-"))
             return false;
         return true;
     }
 
     //사칙연산
-    public static int applyOp(char op, int b, int a){
+    public static int applyOp(String op, int b, int a){
         switch (op){
-            case '+':
+            case "+":
                 return a + b;
-            case '-':
+            case "-":
                 return a - b;
-            case '*':
+            case "*":
                 return a * b;
-            case '/':
+            case "/":
                 if (b == 0) //나누는 수가 0인 경우 예외처리
                     throw new
                             UnsupportedOperationException("Cannot divide by zero");
                 return a / b;
-            case '%':
+            case "%":
                 return a % b;
         }
         return 0;
@@ -135,7 +162,7 @@ public class ev {
         System.out.print("입력 : ");
         Scanner scanner = new Scanner(System.in);
 
-        String str = scanner.next();
-        System.out.println("결과 : " + eval(str));
+        String strs = scanner.next();
+        System.out.println("결과 : " + eval(strs));
     }
 }
